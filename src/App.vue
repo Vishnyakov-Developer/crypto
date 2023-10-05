7ec43499c25df37ece8d359a6efd6cd373fd78f1
Denis Vishnyakov <denisvishnyakov@MacBook-Air-Denis.local>
2023-10-05 19:42:55 +0700

    5 oct

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
       <section>
         <div class="flex">
           <div class="max-w-xs">
-            <label for="wallet" class="block text-sm font-medium text-gray-700">Тикер {{ ticker }} test</label>
+            <label for="wallet" class="block text-sm font-medium text-gray-700">Тикер {{ ticker }}</label>
             <div class="mt-1 relative rounded-md shadow-md">
-              <input v-model="ticker" type="text" name="wallet" id="wallet"
+              <input v-on:keydown.enter="add" v-model="ticker" type="text" name="wallet" id="wallet"
                 class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                 placeholder="Например DOGE" />
             </div>
@@ -31,7 +31,7 @@
             <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
           </div>
         </div>
-        <button type="button"
+        <button type="button" v-on:click="add"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
           <!-- Heroicon name: solid/mail -->
           <svg class="-ml-0.5 mr-2 h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"
@@ -159,8 +159,14 @@ export default {
       ticker: 'default'
     }
   },
+  methods: {
+    add(evt) {
+      alert(evt.key)
+    }
+  },
   components: {
   }
+
 }
 </script>
 
